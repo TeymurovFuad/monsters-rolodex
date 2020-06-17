4fa6bc800b04ca82f9c7490b605eb1a984ad410a
Fuad Teymurov <fuad.teymurov@github.com>
2020-06-18 01:57:22 +0400

    Monsters map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Akula",
+          id: "1",
+        },
+        {
+          name: "Ejdaha",
+          id: "2",
+        },
+        {
+          name: "Tarantula",
+          id: "3",
+        },
+        {
+          name: "Yakudza",
+          id: "4",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((m) => (
+          <p key={m.id}>{m.name}</p>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
