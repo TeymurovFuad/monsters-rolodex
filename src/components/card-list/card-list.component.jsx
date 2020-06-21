f7291aa2c536068a38d5ffcd6fc1482829be30bb
Fuad Teymurov <fuad.teymurov@github.com>
2020-06-21 19:29:23 +0400

    bug fixed

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./card-list.styles.css";
 import { Card } from "../card/card.component";
 
-export const CardList = (props) => {
+export const CardList = (props) => (
   <div className="card-list">
     {props.monsters.map((m) => (
-      <Card key={m.id} monster={props.m} />
+      <Card key={m.id} monster={m} />
     ))}
-  </div>;
-};
+  </div>
+);
